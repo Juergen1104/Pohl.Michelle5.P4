3f459c09113dc8f2c87faa3e4217a98882798584
jpohl <jpohl@web.de>
2024-10-06 16:52:35 +0200

    Aufgabe 3

--- src/Methoden.java
+++ src/Methoden.java
@@ -1,27 +1,78 @@
-public class Methoden {   
+public class Methoden {
 
     /* *** Aufgabenteil (a) *** */
 
- 
-    public static void testMehrheit(){
-	
+    // Methode zur Bestimmung der Mehrheit unter drei booleschen Werten
+    public static boolean mehrheit(boolean v1, boolean v2, boolean v3) {
+        // Die Mehrheit ist true, wenn mindestens zwei der drei Werte true sind
+        return (v1 && v2) || (v1 && v3) || (v2 && v3);
+    }
+
+    public static void testMehrheit() {
+
+        boolean[] values = {true, false};
+
+        System.out.println("Test für mehrheit:");
+
+        // Dreifachschleife zum Testen aller möglichen Kombinationen von drei booleschen Werten
+        for (boolean v1 : values) {
+            for (boolean v2 : values) {
+                for (boolean v3 : values) {
+                    if (mehrheit(v1, v2, v3)) {
+                        // Ausgabe der Parameterkombinationen, bei denen die Methode mehrheit true zurückliefert
+                        System.out.println("v1 = " + v1 + " v2 = " + v2 + " v3 = " + v3);
+                    }
+                }
+            }
+        }
+        System.out.println();
+
     }
 
     /* *** Aufgabenteil (b) *** */
 
-
-    public static void testMonoton(){
-	int[] a = {-1,0,1};
- 	int[] b = {2,2,2,2};
- 	int[] c = {-1,-2,-3};
- 	int[] d = {3,2};
-	int[] e = {0};
-
-	
+    // Methode zur Überprüfung, ob ein Array monoton steigend ist
+    public static boolean monoton(int[] array) {
+        for (int i = 0; i < array.length - 1; i++) {
+            if (array[i] > array[i + 1]) {
+                return false; // Sobald ein Element größer als das nächste ist, ist die Folge nicht monoton
+            }
+        }
+        return true; // Wenn keine Verletzungen der Monotonie gefunden wurden, ist die Folge monoton
     }
 
-    public static void main(String[] args){
-	testMehrheit();
-	testMonoton();
+    public static void testMonoton() {
+        int[] a = {-1, 0, 1};
+        int[] b = {2, 2, 2, 2};
+        int[] c = {-1, -2, -3};
+        int[] d = {3, 2};
+        int[] e = {0};
+
+        // Zweidimensionales Array, das alle zu testenden Felder enthält
+        int[][] arrays = {a, b, c, d, e};
+
+        System.out.println("Test für monoton:");
+
+        // Schleife durch das zweidimensionale Array
+        for (int[] array : arrays) {
+            // Ausgabe des Arrays
+            System.out.print("Die Folge ");
+            for (int num : array) {
+                System.out.print(num + " ");
+            }
+
+            // Überprüfung, ob die Folge monoton ist, und entsprechende Ausgabe
+            if (monoton(array)) {
+                System.out.println("steigt monoton.");
+            } else {
+                System.out.println("steigt nicht monoton.");
+            }
+        }
+        System.out.println();
+    }
+
+    public static void main(String[] args) {
+        testMehrheit();
+        testMonoton();
     }
 }
